76b6c31fb51a3fa20d67067f0695f3f323f546ad
Danikileitor <focusyi@hotmail.com>
2023-01-31 13:45:46 +0100

    VisionNocturna

--- src/main/java/dnk/dnkforge/NightVision.java
+++ src/main/java/dnk/dnkforge/NightVision.java
@@ -3,13 +3,14 @@ package dnk.dnkforge;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EquipmentSlot;
-//import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
+@EventBusSubscriber(modid = DNKforge.MODID)
 public class NightVision extends Enchantment {
     protected NightVision(Rarity pRarity, EnchantmentCategory pCategory, EquipmentSlot... pApplicableSlots){
         super(pRarity, pCategory, pApplicableSlots);
@@ -22,7 +23,6 @@ public class NightVision extends Enchantment {
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent e){
-        //ItemStack casco = e.player.getItemBySlot(EquipmentSlot.HEAD);
         if (EnchantmentHelper.getEnchantmentLevel(DNKforge.NIGHTVISION.get(), e.player) > 0) {
             MobEffectInstance playerEffect = new MobEffectInstance(MobEffects.NIGHT_VISION, 210, 100, false, false);
             e.player.addEffect(playerEffect);
