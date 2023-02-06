4c4035ac7126ef9d1d6d7ceb2414abcd0b276faf
Danikileitor <focusyi@hotmail.com>
2023-02-06 12:22:32 +0100

    boleto_primitiva tooltip

--- src/main/java/dnk/dnkforge/item/custom/BoletoPrimitiva.java
+++ src/main/java/dnk/dnkforge/item/custom/BoletoPrimitiva.java
@@ -1,15 +1,22 @@
 package dnk.dnkforge.item.custom;
 
 import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Nullable;
 
 import dnk.dnkforge.item.ModItems;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent.Chat;
 
 public class BoletoPrimitiva extends Item {
 
@@ -52,6 +59,17 @@ public class BoletoPrimitiva extends Item {
         }
 
         return super.use(level, player, hand);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        if (Screen.hasShiftDown()) {
+            components.add(Component.translatable("item.dnkforge.boleto_primitiva.info").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.translatable("item.dnkforge.boleto_primitiva.hover").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(stack, level, components, flag);        
     }
 
     public static int generarAleatorio(int min, int max) {
